f0fc24c8129af698a997fbd83b982ce1f5618aa5
Yesid Londoño <yesidlondono342@gmail.com>
2025-10-21 13:22:44 -0500

    feat: refactor configuración de CORS para usar una lista de orígenes

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,9 +8,14 @@ from app.api.routes import auth_routes
 
 app = FastAPI(title='API with FastAPI and MongoDB')
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
